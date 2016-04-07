cb57421ff9b7d56f0d0e29f5cac3fa3cfa8c9322
muccix <muccix@gmail.com>
2016-04-07 15:08:07 +0200

    First working test version.

--- test/test_dataobjects.py
+++ test/test_dataobjects.py
@@ -4,12 +4,10 @@
 Test  dataobjects endpoints
 """
 
-# import os
-# import sys
 from restapi.server import create_app
+import io
 
-# from nose.tools import assert_equals
-# from nose.tools import with_setup
+from nose.tools import assert_equals
 
 __author__ = 'Roberto Mucci (r.mucci@cineca.it)'
 
@@ -17,18 +15,26 @@ __author__ = 'Roberto Mucci (r.mucci@cineca.it)'
 class TestDataObjects(object):
 
     @classmethod
-    def setup_class(cls):
+    def setup_class(self):
         "set up test fixtures"
-        print('*******************ciao')
-        cls.app = create_app().test_client()
-        cls.app.config['TESTING'] = True
+        print('### Inside fixture ###')
+        app = create_app()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
     @classmethod
-    def teardown_class(cls):
+    def teardown_class(self):
         "tear down test fixtures"
 
-    #@with_setup(setup_func, teardown_func)
-    def test_get_dataobjects(self):
-        """ GET dataobjects """
+    def test_get_verify(self):
+        """ Test that the flask server is running and reachable"""
         r = self.app.get('http://localhost:8080/api/verify')
-        print (r.text)
+        print(r.status_code)
+        assert_equals(r.status_code, 200)
+
+    def test_post_dataobjects(self):
+        """ Test file upload """
+        r = self.app.post('http://localhost:8080/api/dataobjects', data=dict(
+        file=(io.BytesIO(b"this is a test"), 'test1.pdf'),))
+        print(r.status_code)
+        assert_equals(r.status_code, 200)
