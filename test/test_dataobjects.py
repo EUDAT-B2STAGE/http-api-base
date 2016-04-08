90e9d680d69b73b91e83063fe287e3d47fac07b3
muccix <muccix@gmail.com>
2016-04-08 11:16:06 +0200

    Added delete test

--- test/test_dataobjects.py
+++ test/test_dataobjects.py
@@ -32,18 +32,18 @@ class TestDataObjects(object):
 #        print(r.status_code)
         assert_equals(r.status_code, 200)
 
-    def test_post_dataobjects_light(self):
-        """ Test light file upload """
+    def test_post_dataobjects(self):
+        """ Test file upload """
         # I need to understand who to reapeat the upload test, since
         # overwrite is not allowed
         r = self.app.post('http://localhost:8080/api/dataobjects', data=dict(
                          file=(io.BytesIO(b"this is a test"),
-                          'test_light.pdf')))
+                          'test.pdf')))
 #        print(r.status_code)
         assert_equals(r.status_code, 200)
 
-    def test_post_dataobjects_heavy(self):
-        """ Test heavy file upload """
+    '''def test_post_dataobjects_file(self):
+        """ Test real file upload """
         # I need to understand who to reapeat the upload test, since
         # overwrite is not allowed
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -51,5 +51,14 @@ class TestDataObjects(object):
         r = self.app.post('http://localhost:8080/api/dataobjects', data=dict(
                          file=(open(path, 'rb'), 'img.JPG'')))
 
+#        print(r.status_code)
+        assert_equals(r.status_code, 200)'''
+
+    def test_delete_dataobjects(self):
+        """ Test file delete """
+        deleteURI = os.path.join('http://localhost:8080/api/dataobjects',
+                                 'test.pdf')
+        r = self.app.delete(deleteURI,
+                            data=dict(colletion=('/tempZone/home/rods')))
 #        print(r.status_code)
         assert_equals(r.status_code, 200)
