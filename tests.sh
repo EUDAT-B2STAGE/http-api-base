947f9355852376917061631d6e12b8bf6aaaf646
pdonorio <tempo.pause@gmail.com>
2016-05-04 13:55:06 +0200

    Fixing nose2 tests

--- tests.sh
+++ tests.sh
@@ -3,12 +3,10 @@
 echo "Launching tests with coverage"
 sleep 2
 
-<<<<<<< HEAD
-nosetests \
-    --stop \
-    --with-coverage \
-    --cover-erase --cover-package=restapi \
-    --cover-html --cover-html-dir=/tmp/coverage
-=======
-nose2 --with-coverage
->>>>>>> f3ec335e6aba7c39086c10cebaef75ab9bf49352
+# nosetests \
+#     --stop \
+#     --with-coverage \
+#     --cover-erase --cover-package=restapi \
+#     --cover-html --cover-html-dir=/tmp/coverage
+
+nose2 --with-coverage --stop
