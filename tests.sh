742cf1c80733be7860b9dc083c5a181fe3026792
muccix <muccix@gmail.com>
2016-05-04 13:40:00 +0200

    Tests with nose2 instead of nosetests

--- tests.sh
+++ tests.sh
@@ -3,6 +3,4 @@
 echo "Launching tests with coverage"
 sleep 2
 
-nosetests --with-coverage \
-    --cover-erase --cover-package=restapi \
-    --cover-html --cover-html-dir=/tmp/coverage
+nose2 --with-coverage
