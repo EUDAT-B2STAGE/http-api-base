3f51b307c62dd463032d89a41bb1e1fa1387565c
pdonorio <tempo.pause@gmail.com>
2016-05-24 00:40:40 +0200

    Working some tests

--- test/base/test_restapi.py
+++ test/base/test_restapi.py
@@ -11,7 +11,8 @@ import logging
 import restapi.htmlcodes as hcodes
 from restapi.server import create_app
 from restapi import get_logger, myself
-from confs.config import TEST_HOST, SERVER_PORT, API_URL, AUTH_URL
+from confs.config import USER, PWD, \
+    TEST_HOST, SERVER_PORT, API_URL, AUTH_URL
 
 __author__ = myself
 logger = get_logger(__name__)
@@ -21,7 +22,7 @@ API_URI = 'http://%s:%s%s' % (TEST_HOST, SERVER_PORT, API_URL)
 AUTH_URI = 'http://%s:%s%s' % (TEST_HOST, SERVER_PORT, AUTH_URL)
 
 
-class TestDataObjects(unittest.TestCase):
+class TestRestAPI(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -34,11 +35,35 @@ class TestDataObjects(unittest.TestCase):
     def tearDownClass(self):
         logger.debug('### Tearing down the Flask server ###')
 
-    def test_01_get_verify(self):
+    def test_01_get_status(self):
         """
         Test that the flask server is running and reachable
         """
 
         logger.info("Verify if API is online")
         r = self.app.get(API_URI + '/status')
+        self.assertEqual(r.status_code, hcodes.HTTP_OK_BASIC)
+
+# https://github.com/kelsmj/flask-test-example/blob/master/tests/testusers.py
+
+# Create a user
+
+# Login
+
+# Token
+
+# Logout
+
+# Delete user
+
+    def test_03_get_login(self):
+        """
+        Test that the flask server is running and reachable
+        """
+
+        logger.info("Verify credentials")
+        import json
+        r = self.app.post(
+            AUTH_URI + '/login',
+            data=json.dumps({'username': USER, 'password': PWD}))
         self.assertEqual(r.status_code, hcodes.HTTP_OK_BASIC)
