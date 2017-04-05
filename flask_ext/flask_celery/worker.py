31522ee9c0c3c2458482cd1b3a86d1b8273f06e6
Mattia D'Antonio <m.dantonio@cineca.it>
2017-04-05 12:04:28 +0200

    Trying to extend the of celery ext to worker side

---
+++ flask_ext/flask_celery/worker.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+Celery pattern. Some interesting read here:
+
+http://blog.miguelgrinberg.com/post/celery-and-the-flask-application-factory-pattern
+
+Of course that discussion is not enough for
+a flask templating framework like ours.
+So we made some improvement along the code.
+
+"""
+
+from rapydo.server import create_app
+# from rapydo.services.celery.celery import celery_app
+from rapydo.utils.meta import Meta
+from rapydo.utils.logs import get_logger
+from rapydo.services.detect import services
+# from rapydo.services.detect import services_classes as injectable_services
+# from flask_ext.flask_celery import CeleryExt
+# from injector import inject
+
+log = get_logger(__name__)
+
+################################################
+# Reload Flask app code also for the worker
+# This is necessary to have the app context available
+app = create_app(worker_mode=True)
+
+# Recover celery app with current app
+# celery_app = MyCelery(app)._current
+
+# celery_app = MyCelery(app)._current
+
+# log.pp(injectable_services['celery'].__dict__)
+
+celery_injector = services.get('celery')(app)
+cls, ext = celery_injector.custom_configure()
+
+celery_app = ext.connect()
+
+log.pp(celery_app)
+# with app.app_context() as ctx:
+#     class CeleryWorker(Resource):
+
+#         @inject(**injectable_services)
+#         def __init__(self, **injected_services):
+
+#             log.pp(injected_services)
+
+#     CeleryWorker()
+    # def get_connection():
+
+    #     # ??.custom_connection()
+    #     return None
+
+    # celery_app = get_connection()
+
+    # log.debug("Celery %s" % celery_app)
+
+################################################
+# Import tasks modules to make sure all tasks are avaiable
+
+meta = Meta()
+# main_package = "commons.tasks."
+# # Base tasks
+# submodules = meta.import_submodules_from_package(main_package + "base")
+# # Custom tasks
+# submodules = meta.import_submodules_from_package(main_package + "custom")
